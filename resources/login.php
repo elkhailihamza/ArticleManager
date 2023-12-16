be532fe46e95df1dd33be4ac57ad5f9cb88797a7
elkhailihamza <elkhailihamza331@gmail.com>
2023-12-16 19:11:20 +0100

    created register form, added links in login.php to allow access to register page

--- resources/login.php
+++ resources/login.php
@@ -3,7 +3,7 @@ include(__DIR__ . "/../includes/reg_header.php");
 ?>
 
 <main class="container-fluid d-flex flex-column justify-content-center align-items-center">
-    <div class="container col-6 d-grid gap-3 mt-2">
+    <div class="container col-6 d-grid gap-3 mt-4">
         <div class="text-center">
             <h2>
                 Sign-in
@@ -13,12 +13,16 @@ include(__DIR__ . "/../includes/reg_header.php");
             <div class="container d-grid gap-3 mt-5" style="width: 425px;">
                 <div class="form-floating">
                     <input class="form-control" type="email" placeholder="Enter user email.." maxlength="125"
-                        name="title" required></input>
+                        name="email" required></input>
                     <label>Email</label>
                 </div>
                 <div class="form-floating">
-                    <input class="form-control" type="password" placeholder="Enter user password.." required>
+                    <input class="form-control" type="password" placeholder="Enter user password.." name="password" maxlength="125" required>
                     <label>Password</label>
+                </div>
+                <div class="container d-grid">
+                    <span>Haven't got an account? <a href="./register.php">sign-up</a> now!</span>
+                    <span><a href="#">forgot password?</a></span>
                 </div>
                 <div class="container d-flex justify-content-center align-items-center mt-5">
                     <button class="btn btn-primary" style="width: 150px;" type="submit" name="submit"
